eb24b6e8dc7676088b52dc107d5581e6c24d6ce4
debagger <debagger@gmail.com>
2019-10-28 21:36:31 +0300

    Initial commit

---
+++ packages/rent-a-dress/server.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const fs = require("fs");
+const path = require('path')
+
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true,
+  https: {
+    key: fs.readFileSync("./ssl/key.txt"),
+    cert: fs.readFileSync("./ssl/www_rent_a_dress_ru_2020_07_10.crt")
+  }
+})
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'dist')
+})
+
+fastify.register(require("fastify-https-redirect"));
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.sendFile('index.html')
+})
+
+// Run the server!
+fastify.listen(443, '0.0.0.0', function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
---
+++ packages/rent-a-dress/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Home />
+  </div>
+</template>
+
+<script>
+import "@storefront-ui/vue/styles.scss";
+import { Home } from "@storefront-ui/vue";
+
+export default {
+  name: "app",
+  components: {
+    Home
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
