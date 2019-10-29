4692369d7dd31088102957abeb678a932b402325
debagger <debagger@gmail.com>
2019-10-29 22:33:43 +0500

    sequre server enabled

--- packages/rent-a-dress/server.js
+++ packages/rent-a-dress/server.js
@@ -6,28 +6,17 @@ const path = require('path')
 // Require the framework and instantiate it
 const fastify = require('fastify')({
   logger: true,
-<<<<<<< HEAD
-  // https: {
-  //   key: fs.readFileSync("./ssl/key.txt"),
-  //   cert: fs.readFileSync("./ssl/www_rent_a_dress_ru_2020_07_10.crt")
-  // }
-=======
   https: {
     key: fs.readFileSync("./ssl/key.txt"),
     cert: fs.readFileSync("./ssl/www_rent_a_dress_ru_2020_07_10.crt")
   }
->>>>>>> 5298847182fc25b6ab9355ec4c1001df85325b19
 })
 
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'dist')
 })
 
-<<<<<<< HEAD
-// fastify.register(require("fastify-https-redirect"));
-=======
 fastify.register(require("fastify-https-redirect"));
->>>>>>> 5298847182fc25b6ab9355ec4c1001df85325b19
 
 // Declare a route
 fastify.get('/', function (request, reply) {
@@ -35,11 +24,7 @@ fastify.get('/', function (request, reply) {
 })
 
 // Run the server!
-<<<<<<< HEAD
-fastify.listen(80, '0.0.0.0', function (err, address) {
-=======
 fastify.listen(443, '0.0.0.0', function (err, address) {
->>>>>>> 5298847182fc25b6ab9355ec4c1001df85325b19
   if (err) {
     fastify.log.error(err)
     process.exit(1)
