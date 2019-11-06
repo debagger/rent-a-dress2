0b69c6ec11673e238923588a2ae86aec6eb5a6e6
debagger <debagger@gmail.com>
2019-11-06 23:25:28 +0500

    Change to nuxt

--- packages/rent-a-dress/server.js
+++ packages/rent-a-dress/server.js
@@ -1,67 +1,82 @@
 "use strict";
+const { Nuxt, Builder } = require("nuxt");
+const nuxtConfig = require("./nuxt.config");
+const nuxt = new Nuxt(nuxtConfig);
+const builder = new Builder(nuxt);
 
-const fs = require("fs");
-const path = require("path");
-const exec = require("child_process").exec;
+builder.build().then(() => {
+  const fs = require("fs");
+  const path = require("path");
+  const exec = require("child_process").exec;
 
-// Require the framework and instantiate it
-const fastify = require("fastify")({
-  logger: true,
-  https: {
-    key: fs.readFileSync("./ssl/key.txt"),
-    cert: fs.readFileSync("./ssl/www_rent_a_dress_ru_2020_07_10.crt")
-  }
-});
-
-fastify.register(require("fastify-static"), {
-  root: path.join(__dirname, "client/dist")
-});
-
-fastify.register(require("fastify-https-redirect"));
-// Enable the fastify CORS plugin
-// fastify.register(require('fastify-cors'), {
-//   origin: '*',
-//   credentials: true
-// })
-
-// Declare a route
-fastify.get("/", function(request, reply) {
-  reply.sendFile("index.html");
-});
-
-fastify.post("/webhook", function(request, reply) {
-  reply.send();
-  exec("git pull", function(error, stdout, stderr) {
-    if (error) {
-      fastify.log.error(error);
+  // Require the framework and instantiate it
+  const fastify = require("fastify")({
+    logger: true,
+    https: {
+      key: fs.readFileSync("./ssl/key.txt"),
+      cert: fs.readFileSync("./ssl/www_rent_a_dress_ru_2020_07_10.crt")
     }
-    if (stdout) {
-      fastify.log.info(stdout);
-    }
-    if (stderr) {
-      fastify.log.error(stderr);
-    }
-    fastify.log.warn("Restarting server after git pull...");
-    fastify.close();
-    process.exit();
   });
-});
 
-fastify.get("/items", function(request, reply) {
-  const files = JSON.stringify(
-    fs.readdirSync("./public/assets/items", "utf-8")
-  );
-  reply
-    .code(200)
-    .header("Content-Type", "application/json; charset=utf-8")
-    .send(files);
-});
+  fastify.use(nuxt.render);
 
-// Run the server!
-fastify.listen(443, "0.0.0.0", function(err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`server listening on ${address}`);
+  fastify.register(require("fastify-static"), {
+    root: path.join(__dirname, "static")
+  });
+
+  fastify.register(require("fastify-https-redirect"));
+
+  fastify.get("/_nuxt/*", (request, reply) => {
+    reply.sent = true;
+    let rq = request.raw;
+    return nuxt.render(rq, reply.res);
+  });
+
+  fastify.route({
+    url: "/__webpack_hmr/*",
+    method: ["DELETE", "GET", "HEAD", "PATCH", "POST", "PUT", "OPTIONS"],
+    handler: (request, reply) => {
+      reply.sent = true;
+      let rq = request.raw;
+      return nuxt.render(rq, reply.res);
+    }
+  });
+
+  // Enable the fastify CORS plugin
+  // fastify.register(require('fastify-cors'), {
+  //   origin: '*',
+  //   credentials: true
+  // })
+
+  // Declare a route
+  // fastify.get("/", function(request, reply) {
+  //   reply.sendFile("index.html");
+  // });
+
+  fastify.post("/webhook", function(request, reply) {
+    reply.send();
+    exec("git pull", function(error, stdout, stderr) {
+      if (error) {
+        fastify.log.error(error);
+      }
+      if (stdout) {
+        fastify.log.warn(stdout);
+      }
+      if (stderr) {
+        fastify.log.error(stderr);
+      }
+      fastify.log.warn("Restarting server after git pull...");
+      fastify.close();
+      process.exit();
+    });
+  });
+
+  // Run the server!
+  fastify.listen(443, "0.0.0.0", function(err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    fastify.log.info(`server listening on ${address}`);
+  });
 });
