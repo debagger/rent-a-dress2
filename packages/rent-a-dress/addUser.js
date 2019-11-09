99c2988f90836fba491ae70f4eae5ce5f7dbc716
debagger <debagger@gmail.com>
2019-11-10 02:23:51 +0500

    Fix bugs

--- packages/rent-a-dress/addUser.js
+++ packages/rent-a-dress/addUser.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const Db = require("tingodb")().Db;
 
-
 const dataDir = "./data";
 
 fs.mkdirSync(dataDir, { recursive: true });
@@ -9,7 +8,17 @@ fs.mkdirSync(dataDir, { recursive: true });
 const db = new Db(dataDir, {});
 const Users = db.collection("users");
 
-Users.insert([{username:"admin", password:"123", email:"debagger@gmail.com"}],function(err, item){
-    if(item) console.log(item);
-    if(err) console.error(err);
-})
+Users.insert(
+  [
+    {
+      username: "admin",
+      password: "123",
+      email: "debagger@gmail.com",
+      role: "admin"
+    }
+  ],
+  function(err, item) {
+    if (item) console.log(item);
+    if (err) console.error(err);
+  }
+);
