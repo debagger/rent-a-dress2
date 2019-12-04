d25894c94f26a1c35c23543ad7773def8c5ed020
debagger <debagger@gmail.com>
2019-12-04 22:16:59 +0500

    Project updated to Typescript

--- packages/rent-a-dress/src/entity/Image.ts
+++
@@ -1,8 +0,0 @@
-import { IsString, IsInt } from "class-validator";
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
-@Entity()
-export class Image {
-  @PrimaryGeneratedColumn() @IsInt() id: number;
-  @Column() @IsString() imageName: String;
-}
