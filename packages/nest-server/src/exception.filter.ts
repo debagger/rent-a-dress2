9c603710423de1efeee44e17aa08a1338532694f
Mikhail Sokolov <debagger@gmail.com>
2020-05-11 21:13:13 +0500

    Fix no return message field in some cases

--- packages/nest-server/src/exception.filter.ts
+++ packages/nest-server/src/exception.filter.ts
@@ -8,25 +8,31 @@ import {
 import { Request, Response } from 'express';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-export class ExceptionResponse{
-  @ApiProperty() statusCode: number
-  @ApiProperty() path: string
-  @ApiPropertyOptional() message?: string
+export class ExceptionResponse {
+  @ApiProperty() statusCode: number;
+  @ApiProperty() path: string;
+  @ApiPropertyOptional() message?: string;
 }
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+
     const response = ctx.getResponse<Response>();
+
     const request = ctx.getRequest<Request>();
+
     const status = exception.getStatus ? exception.getStatus() : 500;
 
     const responseData: ExceptionResponse = {
       statusCode: status,
       path: request.url,
-      message: exception?.message?.message
-    }
+      message: exception?.message?.message,
+    };
+    responseData.message = responseData.message
+      ? responseData.message
+      : exception.toString();
 
     response.status(status).json(responseData);
   }
